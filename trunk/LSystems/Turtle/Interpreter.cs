581d98d07d556d1f8480816140811176052dca33
pavlov_7@yahoo.de <pavlov_7@yahoo.de@2924d877-c995-2a61-b84c-a58990ebd0c4>
2011-06-10 22:44:56 +0000

    Added Turtle basics - modules, interface and simple interpreter.

---
+++ trunk/LSystems/Turtle/Interpreter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace LSystems.Turtle
+{
+    public class Interpreter
+    {
+        private Dictionary<Type, Action<object>> registeredTypes = new Dictionary<Type,Action<object>>();
+
+        public ITurtle Turtle { get; set; }
+        
+        public void Register<T>(Action<T> action)
+        {
+            registeredTypes[typeof(T)] = p => action((T)p);
+        }
+
+        public Interpreter()
+        {
+            Register<StartBranchModule>(p => Turtle.Push());
+            Register<EndBranchModule>(p => Turtle.Pop());
+            Register<F>(p => Turtle.Forward(p.Value, true));
+            Register<f>(p => Turtle.Forward(p.Value, false));
+            Register<L>(p => Turtle.Turn(p.Value));
+            Register<R>(p => Turtle.Turn(-p.Value));
+            Register<MoveTo>(p => Turtle.Move(p.Value.X, p.Value.Y));
+            Register<MoveToRel>(p => Turtle.MoveRel(p.Value.X, p.Value.Y));
+            Register<LineThickness>(p => Turtle.LineThickness(p.Value));
+            Register<LineColor>(p => Turtle.LineColor(p.Value.R, p.Value.G, p.Value.B));
+        }
+
+        public void Interpret(ITurtle turtle, IEnumerable modules)
+        {
+            foreach (var module in modules)
+            {
+                Action<object> action;
+                if (registeredTypes.TryGetValue(module.GetType(), out action))
+                {
+                    action(module);
+                }
+            }
+        }
+    }
+}
---
+++ trunk/LSystems/Turtle/Modules.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LSystems.Turtle
+{
+    public class Module<T>
+    {
+        public T Value { get; protected set; }
+
+        protected Module(T value)
+        {
+            this.Value = value;
+        }
+
+        protected Module()
+        {
+        }
+    }
+
+    public class F : Module<double>
+    {
+        public F(double distance) : base(distance) { }
+        public F() { }    
+    }
+
+    public class f : Module<double>
+    {
+        public f(double distance) : base(distance) { }
+        public f() { }
+    }
+
+    public class L : Module<double>
+    {
+        public L(double angle) : base(angle) { }
+        public L() { }        
+    }
+
+    public class R : Module<double>
+    {
+        public R(double angle) : base(angle) { }
+        public R() { }        
+    }
+
+    public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+
+    public class MoveTo : Module<Point>
+    {
+        public MoveTo(double x, double y) : base(new Point() { X = x, Y = y }) { }
+        public MoveTo() { }        
+    }
+
+    public class MoveToRel : Module<Point>
+    {
+        public MoveToRel(double x, double y) : base(new Point() { X = x, Y = y }) { }
+        public MoveToRel() { }        
+    }
+
+    public struct Color
+    {
+        public double R { get; set; }
+        public double G { get; set; }
+        public double B { get; set; }
+    }
+
+    public class LineColor : Module<Color>
+    {
+        public LineColor(double r, double g, double b)
+            : base(new Color() { R = r, G = g, B = b }) 
+        {
+        }
+        public LineColor() { }        
+    }
+
+    public class LineThickness : Module<double>
+    {
+        public LineThickness(double thickness) : base(thickness) { }
+        public LineThickness() { }
+    }
+}
